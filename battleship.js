f57118a73af04c13df8c742cea204710dea13542
jlopez1423 <jlopez46@ucsc.edu>
2016-09-26 21:00:41 -0700

    Day 25 of git Challenge
    Did some debugging

--- battleship.js
+++ battleship.js
@@ -1,6 +1,6 @@
 var location1 = 3; 
 var location2 = 4; 
-var location3 = 5 
+var location3 = 5;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -10,12 +10,26 @@ var isSunk = false;
 
 while(isSunk == false){
 	//User's guess
-	guesses = prompt("Ready, aim, fire!(Enter a number from 0 - 6:");
+	guess = prompt("Ready, aim, fire!Enter a number from 0 - 6:");
 
 	if(guess < 0 || guess > 6){
 		alert("Please enter a valid response.");
 	} else {
 		guesses = guesses + 1;  
 	}
+	if(guess == location1 || guess == location2 || guess == location3){
+		hits = hits + 1;
+		alert("You have a hit");		
+		if( hits == 3){
+		isSunk = true;
+		alert("You sunk my battleship!");
+		}
+	} else { alert("you have a miss");}
 
+	
 }
+
+
+var stats = "You took " + guesses + " gueses to sink the battleship, " + 
+"which means your shoorint accuracy was " + (3/guesses);
+alert(stats);
